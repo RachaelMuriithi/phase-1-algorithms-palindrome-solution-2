ab01084bafcf09173bebd4262c7732b50a757bfa
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-08-27 23:11:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // iterate from beginning the middle
+  for (let i = 0; i < word.length / 2; i++) {
+    //check each letter to the corresponding last letter
+    const j = word.length - 1 - i;
+    // compare using strict inequality operator
+    if (word[i] !== word[j]) {
+      // if the letters dont match return false
+
+      return false;
+    }
+  }
+  // if all the letters match all the way upto the center
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  iterate from the beginning to the end of the word
+  check each letter in the first to the corresponding last character
+  if letters don't match return false ie not palindrome
+  if letters match return true ie is a palindrome
 */
 
 /*
@@ -18,8 +35,18 @@ if (require.main === module) {
 
   console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("oppo"));
+
+  console.log("");
+
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("dog"));
 }
 
 module.exports = isPalindrome;
